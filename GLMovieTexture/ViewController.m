7edfecce39ceb1518a1baf02104d8be32e42611b
hayashi <hayashi@qoncept.jp>
2012-12-22 21:45:14 +0900

    Initial Commit

---
+++ GLMovieTexture/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GLMovieTexture
+//
+//  Created by hayashi on 12/22/12.
+//  Copyright (c) 2012 hayashi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GLMovieTexture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLMovieTexture
+//
+//  Created by hayashi on 12/22/12.
+//  Copyright (c) 2012 hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
