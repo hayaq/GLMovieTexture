612324de33cf548963da7deef1c9cb150a7c92cf
hayashi <hayashi@qoncept.jp>
2012-12-28 20:38:12 +0900

    aaaa

--- Unity2/MovieTexture/Assets/Example1/Behaviour1.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class Behaviour1 : MonoBehaviour {
-	void Update () {
-		this.transform.localRotation = Quaternion.AngleAxis(20.0f*Time.time,Vector3.up);	
-	}
-}
